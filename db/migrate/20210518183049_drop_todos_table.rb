9f37a50ba3af6f7a79cbde22916caaad65519650
alexis zaffalon <alexiszaffalon@live.fr>
2021-05-18 20:34:08 +0200

    delete Todo model & drop todos table

---
+++ db/migrate/20210518183049_drop_todos_table.rb
@@ -0,0 +1,5 @@
+class DropTodosTable < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :todos
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_152916) do
+ActiveRecord::Schema.define(version: 2021_05_18_183049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.boolean "completed", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
